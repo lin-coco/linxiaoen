dc8b584372e24ec632d9d6ca4646a8a2ec594777
link_xue <2879322503@qq.com>
2022-10-17 07:05:47 +0800

    林小恩 虚拟机器人之旅

---
+++ src/main/java/com/lincoco/linxiaoen/LinxiaoenApplication.java
@@ -0,0 +1,13 @@
+package com.lincoco.linxiaoen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinxiaoenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinxiaoenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lincoco/linxiaoen/LinxiaoenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincoco.linxiaoen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinxiaoenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
